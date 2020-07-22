260d00864c5342d2de7a8d95160a92d9d88bb4df
oscarmora26 <oscar.rander@outlook.com>
2020-07-22 01:47:52 -0400

    Sidebar Administrador 1.1

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -21,7 +21,7 @@ namespace Tienda_.Controllers
         {
             return View();
         }
-
+        
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
