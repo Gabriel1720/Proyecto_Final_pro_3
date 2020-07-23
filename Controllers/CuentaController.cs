15812a06e7aaac69601ba08fcb23704ed89f1c95
Felix Jose <felixjoseynfantequezada@gmail.com>
2020-07-22 23:31:51 -0400

    Agregando Cuenta Administrador

--- Controllers/CuentaController.cs
+++ Controllers/CuentaController.cs
@@ -6,24 +6,29 @@ namespace Tienda_.Controllers
     public class CuentaController : Controller
     {
       
-    public IActionResult Login(){
+        public IActionResult Login()
+        {
+            return View(); 
+        }
 
-       return View(); 
-    }
+        public IActionResult Nueva_cuenta() 
+        {
+            return View(); 
+        }
 
-    public IActionResult Nueva_cuenta() {
-        return View(); 
-    }
-
-    public IActionResult Registrarse() {
-        return View();
-    }
-    
-    public IActionResult Detalle_Producto()
-    {
+        public IActionResult Registrarse() 
+        {
             return View();
-    }
+        }
+    
+        public IActionResult Detalle_Producto()
+        {
+                return View();
+        }
 
-         
+        public IActionResult Cuenta_Administrador()
+        {
+            return View();
+        }  
     }
 }
