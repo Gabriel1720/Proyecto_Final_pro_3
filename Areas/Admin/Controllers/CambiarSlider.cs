ba2d93603bf6e7e00912a2a0eb279e6d7825008f
Felix Jose <felixjoseynfantequezada@gmail.com>
2020-07-29 13:35:16 -0400

    Agregando vista Cambiar Slider

---
+++ Areas/Admin/Controllers/CambiarSlider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Proyecto_final_pro_3.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CambiarSlider : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
