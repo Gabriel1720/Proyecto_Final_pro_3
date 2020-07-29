2cb4899b7687abbe417fc84f153fe2f4db838e56
oscarmora26 <oscar.rander@outlook.com>
2020-07-29 00:43:33 -0400

    apartado de reporte incompleto

--- Areas/Admin/Controllers/GestionReportes.cs
+++ Areas/Admin/Controllers/GestionReportes.cs
@@ -3,19 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Proyecto_final_pro_3.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Proyecto_final_pro_3.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class GestionReportes : Controller
     {
-        public IActionResult Index()
+        DB_A64A4C_SuperMercadoContext _context = new DB_A64A4C_SuperMercadoContext();
+
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var orden = await _context.Orden.Include(x => x.IdUsuarioNavigation).ToListAsync();
+            return View(orden); 
         }
 
-        public IActionResult DetalleCompra()
+        public async Task<IActionResult> Details(int? id)
         {
+            ViewBag.order = await _context.Orden.Include(x => x.IdUsuarioNavigation).Include(X => X.DetalleOrden).FirstOrDefaultAsync(x => x.IdOrden == id);
+            ViewBag.Detalle_orden = await _context.DetalleOrden.Include(x => x.IdProductoNavigation).Where(x => x.IdOrden == id).ToListAsync();
             return View();
         }
     }
