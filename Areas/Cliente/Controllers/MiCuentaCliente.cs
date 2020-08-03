3a6ed7874cb0790e549d2964351db1de804348fd
EironDiaz <eirondiaz@gmail.com>
2020-08-03 14:47:53 -0400

    pedidos tabla

--- Areas/Cliente/Controllers/MiCuentaCliente.cs
+++ Areas/Cliente/Controllers/MiCuentaCliente.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Proyecto_final_pro_3.Models;
 
 namespace Proyecto_final_pro_3.Areas.Cliente.Controllers
 {
     [Area("Cliente")]
     public class MiCuentaCliente : Controller
     {
+        public DB_A64A4C_SuperMercadoContext CT = new DB_A64A4C_SuperMercadoContext();
         public IActionResult Index()
         {
             return View();
@@ -16,7 +18,6 @@ namespace Proyecto_final_pro_3.Areas.Cliente.Controllers
 
         public IActionResult Ordenes()
         {
-
             return View();
         }
     }
