789e59c5b1d7704aeadd247133ded9d958e3caf9
Felix Jose <felixjoseynfantequezada@gmail.com>
2020-08-02 23:32:05 -0400

    Agregando vista MiCuentaCliente

---
+++ Areas/Cliente/Controllers/MiCuentaCliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Proyecto_final_pro_3.Areas.Cliente.Controllers
+{
+    [Area("Cliente")]
+    public class MiCuentaCliente : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
