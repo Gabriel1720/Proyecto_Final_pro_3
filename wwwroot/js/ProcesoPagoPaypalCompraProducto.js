8edf5147ffa5e077665b520b2b243e4bb4b2567a
Gabriel <sr.gabrielgil@gmail.com>
2020-08-05 10:32:26 -0400

    merjoras v1.0.6

--- wwwroot/js/ProcesoPagoPaypalCompraProducto.js
+++ wwwroot/js/ProcesoPagoPaypalCompraProducto.js
@@ -124,8 +124,8 @@ function showPosition(position) {
 
     var map = L.map('map').setView([latitud, longitud], 14);
 
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-        attribution: '',
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
         maxZoom: 30,
         id: 'mapbox/streets-v11',
         tileSize: 512,
